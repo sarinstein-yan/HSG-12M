991e1e67a846252663717ecbbef39fa9a375ff9e
Xianquan (Sarinstein) Yan <98195226+sarinstein-yan@users.noreply.github.com>
2025-09-22 11:01:58 +0800

    Update init_env.sh

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -1,9 +1,9 @@
-echo "Pip installing torch=2.8.0, torchvision=0.21.0 for CUDA=$CUDA ..."
-pip install torch==2.8.0 torchvision==0.21.0 \
+echo "Pip installing torch=2.8.0, torchvision=0.23.0 for CUDA=$CUDA ..."
+pip install torch==2.8.0 torchvision==0.23.0 \
             --index-url https://download.pytorch.org/whl/${CUDA}
 
 echo "Pip installing lightning, torch_geometric ..."
-pip install lightning
+pip install lightning==2.5.4
 pip install torch_geometric
 pip install torch_scatter torch_cluster torch_spline_conv \
         -f https://data.pyg.org/whl/torch-2.8.0+${CUDA}.html
